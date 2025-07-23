2459e063bba04c5d56ec687defe55587217a891f
Liudmyla Klochko <klochko.lyuda@gmail.com>
2025-07-23 12:10:43 +0200

    add:spe calcs

--- run_SAE.py
+++ run_SAE.py
@@ -1,11 +1,41 @@
-
 #%%
 import torch, sys, json
-import utils_f as u
-import pickle
+import SAE.sparce_autoencoder as SAE
 torch.backends.cudnn.enabled=False
-from tqdm import tqdm
 import pandas as pd
+
+def trainSAE(SAEs, optimizers, layer, acts, SAEevs):
+    if layer not in SAEs:
+        factor = config["basefactor"] 
+        while acts.size()[1]*acts.size()[1]*factor*4 > config["maxsize"]:
+            factor -= 1
+        if factor < 1: 
+            SAEs[layer] = None   
+            print(f"{layer} too large, skipping")
+        else:    
+            encoding_dim = int(acts.size()[1] * factor)
+            print(f"Encoding dim for {layer} = {encoding_dim} (factor={factor})")
+            SAEs[layer] = SAE.SparseAutoencoder(acts.size()[1], 
+                                                encoding_dim, 
+                                                beta=config["beta"], 
+                                                rho=config["rho"]).to(device) 
+            SAEev[layer] = {"rec": 0, "sparse": 0, "loss": 0, "min": None}
+            optimizers[layer] = torch.optim.Adam(SAEs[layer].parameters(), 
+                                                 lr=config["learningrate"])
+            SAEs[layer].train()
+    if SAEs[layer] is None: return
+    optimizers[layer].zero_grad()
+    decoded, encoded = SAEs[layer](acts)
+    total_loss, recon_loss_val, sparsity_val = SAEs[layer].compute_loss(acts, decoded, encoded)
+    total_loss.backward()
+    optimizers[layer].step()
+    SAEev[layer]["rec"] += recon_loss_val
+    SAEev[layer]["sparse"] += sparsity_val
+    SAEev[layer]["loss"] += total_loss
+
+
+
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if device == torch.device("cuda"): print("USING GPU")
@@ -17,14 +47,13 @@ torch.manual_seed(torchseed)
 torch.cuda.manual_seed(torchseed)
 
 
-print("********  Loading dataset  ********")
+print("********  Loading activations  ********")
 
 batch = torch.load('../activations_MEGNet/activations.pt')
 possible_layers = pd.read_csv('../activations_MEGNet/non_empty_layers.txt')
 
-layer = possible_layers.layers[1]
-
-print ("Train will be done for :",layer)
+#layer = possible_layers.layers[1]
+#print ("Train will be done for :",layer)
 
 print("********  Training SAE  ********")
 
@@ -37,32 +66,9 @@ for ep in range(1, config["nepochs_sae"]+1):
     count = 0
     sev = 0
     # for X,y in data_loader:
-    for i, struct in enumerate(structures):
-        u.activation = {}
-        with torch.no_grad():
-        pred = model.predict_structure(struct)        
-        for layer in u.activation:
-        acts = get_activations_megnet(u.activation[layer])
-        if acts is None or len(acts) <= 1 : continue
-        # print(layer, "::", acts.shape)
-        # create a batch of batch_size
-        if layer not in batch: batch[layer] = []
-        else: 
-            if len(batch[layer]) != 0 and len(acts) != len(batch[layer][0]):
-                # print("Bad layer: ", layer)
-                batch[layer][0] = []
-            else: 
-                batch[layer].append(acts)
-        if len(batch[layer]) >= config["batchsize"]:
-            #print("layer batch", layer) 
-            # print(len(batch[layer]))
-            # trainSOM(torch.stack(batch[layer], dim=0), layer, SOMs, mm, SOMevs)
-            # print(".", end="")
-            trainSAE(SAEs, optimizers, layer, torch.stack(batch[layer], dim=0), SAEev)
-            batch[layer] = []
-        count += 1
-        if i%100==0: print(".", end="")
-    print()        
+    for layer in possible_layers.layers:
+        trainSAE(SAEs, optimizers, layer, torch.stack(batch[layer], dim=0).to(device), SAEev)
+        count = len (batch[layer])
 
     for layer in SAEev:
         torch.save(SAEs[layer], f"{config['saedir']}/{layer}.pkl")
